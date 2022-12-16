956af503a3d44ef74ef93e15e9340dd65e6599d4
carlos <carlos@192.168.1.186>
2022-12-15 18:56:12 -0600

    Agregado Frame Juego

---
+++ src/view/Juego.java
@@ -0,0 +1,32 @@
+/** ****************************************************************************
+ *Autor:Carlos Aurelio Alcántara Pérez
+ *Fecha de creación: 18-11-2022 ***
+ *Fecha de actualización:31-11-2022
+ *Descripción:Panel para la portada 
+ *
+ * ****************************************************************************/
+package view;
+
+/**
+ *
+ * @author Carlos Aurelio Alcántara Pérez
+ */
+public class Juego extends javax.swing.JPanel {
+
+    
+    public Juego() {
+        initComponents();
+    }
+
+  
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
