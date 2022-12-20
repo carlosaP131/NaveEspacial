ee3179a08bbcfac30d03868522fbfe58105ec521
Getzemani <alebk201@gmail.com>
2022-12-20 10:03:15 -0600

    tabla jugador

--- src/Controller/JugadorController.java
+++ src/Controller/JugadorController.java
@@ -11,6 +11,8 @@ import Controller.base.RegistroControllerBase;
 import Service.IJugadorService;
 import Service.JugadorServiceImpl;
 import entity.Jugador;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -33,6 +35,25 @@ public class JugadorController extends RegistroControllerBase<Jugador> {
 
         super.setRegistro(jugador);
         super.crearActualizarRegistro();
+    }
+    
+    
+     public void mostrar(DefaultTableModel modelo) {
+        service = new JugadorServiceImpl();
+        List<Jugador> listajugador = service.ObtenerRegistro();
+        modelo.setRowCount(0);
+        for (Jugador jugador : listajugador) {
+            Object[] fila = new Object[6];
+            fila[0] = String.valueOf(jugador.getIdJugador());
+            fila[1] = jugador.getNombre();
+            fila[2] = jugador.getEdad();
+            fila[3] = jugador.getSexo();
+            fila[4] = jugador.getCorreo();
+            fila[5] = jugador.getPuntos();
+            modelo.addRow(fila);
+
+        }
+
     }
 
 }
