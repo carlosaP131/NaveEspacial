3e486e75569b16af0f9da1a7f0a2933e48fc875b
CARLOS ALCANTARA <carlos.cantrax303@gmail.com>
2023-02-01 16:28:52 -0600

    ya tiene boton reporte

--- src/View/Menu.java
+++ src/View/Menu.java
@@ -1,11 +1,20 @@
 package View;
 
+import javaReportes.Conexion;
+import com.sun.jdi.connect.spi.Connection;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
 public class Menu extends javax.swing.JFrame {
 
     TablaUsuario tablaUsuario;
     RegistrarUsuario registrar;
     TablaJugador tablaJugador;
     Juego juego;
+    private Connection conection;
+    private javaReportes.Conexion conexion;
     int idJugador;
 
     public Menu() {
@@ -25,10 +34,11 @@ public class Menu extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
+        jugar = new javax.swing.JButton();
+        Reporte = new javax.swing.JButton();
+        Consultar = new javax.swing.JButton();
+        Registrar = new javax.swing.JButton();
+        Jugador = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -50,49 +60,70 @@ public class Menu extends javax.swing.JFrame {
         jLabel4.setText("Menu");
         jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 30, -1, -1));
 
-        jButton1.setBackground(new java.awt.Color(204, 204, 204));
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(51, 51, 51));
-        jButton1.setText("Jugar");
-        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+        jugar.setBackground(new java.awt.Color(204, 204, 204));
+        jugar.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jugar.setForeground(new java.awt.Color(51, 51, 51));
+        jugar.setText("Jugar");
+        jugar.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton1MouseClicked(evt);
+                jugarMouseClicked(evt);
             }
         });
-        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 150, 170, -1));
+        jPanel1.add(jugar, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 110, 170, -1));
 
-        jButton3.setBackground(new java.awt.Color(204, 204, 204));
-        jButton3.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
-        jButton3.setForeground(new java.awt.Color(51, 51, 51));
-        jButton3.setText("Jugadores");
-        jButton3.addMouseListener(new java.awt.event.MouseAdapter() {
+        Reporte.setBackground(new java.awt.Color(204, 204, 204));
+        Reporte.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        Reporte.setForeground(new java.awt.Color(51, 51, 51));
+        Reporte.setText("Reporte");
+        Reporte.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton3MouseClicked(evt);
+                ReporteMouseClicked(evt);
             }
         });
-        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 360, 170, -1));
+        Reporte.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ReporteActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Reporte, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 310, 170, -1));
 
-        jButton4.setBackground(new java.awt.Color(204, 204, 204));
-        jButton4.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
-        jButton4.setForeground(new java.awt.Color(51, 51, 51));
-        jButton4.setText("Consultar");
-        jButton4.addMouseListener(new java.awt.event.MouseAdapter() {
+        Consultar.setBackground(new java.awt.Color(204, 204, 204));
+        Consultar.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        Consultar.setForeground(new java.awt.Color(51, 51, 51));
+        Consultar.setText("Consultar");
+        Consultar.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton4MouseClicked(evt);
+                ConsultarMouseClicked(evt);
             }
         });
-        jPanel1.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 220, 170, -1));
+        jPanel1.add(Consultar, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 160, 170, -1));
 
-        jButton5.setBackground(new java.awt.Color(204, 204, 204));
-        jButton5.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
-        jButton5.setForeground(new java.awt.Color(51, 51, 51));
-        jButton5.setText("Registrar");
-        jButton5.addMouseListener(new java.awt.event.MouseAdapter() {
+        Registrar.setBackground(new java.awt.Color(204, 204, 204));
+        Registrar.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        Registrar.setForeground(new java.awt.Color(51, 51, 51));
+        Registrar.setText("Registrar");
+        Registrar.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton5MouseClicked(evt);
+                RegistrarMouseClicked(evt);
             }
         });
-        jPanel1.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 290, 170, -1));
+        Registrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                RegistrarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Registrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 210, 170, -1));
+
+        Jugador.setBackground(new java.awt.Color(204, 204, 204));
+        Jugador.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        Jugador.setForeground(new java.awt.Color(51, 51, 51));
+        Jugador.setText("Jugadores");
+        Jugador.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                JugadorMouseClicked(evt);
+            }
+        });
+        jPanel1.add(Jugador, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 260, 170, -1));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/menu_1.png"))); // NOI18N
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
@@ -120,32 +151,58 @@ public class Menu extends javax.swing.JFrame {
         System.exit(0);
     }//GEN-LAST:event_jLabel3MouseClicked
 
-    private void jButton4MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton4MouseClicked
+    private void ConsultarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ConsultarMouseClicked
         // TODO add your handling code here:
         tablaUsuario=  new TablaUsuario();
         this.setVisible(false);
         tablaUsuario.setVisible(true);
 
-    }//GEN-LAST:event_jButton4MouseClicked
+    }//GEN-LAST:event_ConsultarMouseClicked
 
-    private void jButton5MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton5MouseClicked
+    private void RegistrarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_RegistrarMouseClicked
         // TODO add your handling code here:
         registrar = new RegistrarUsuario();
         this.setVisible(false);
         registrar.setVisible(true);
-    }//GEN-LAST:event_jButton5MouseClicked
+    }//GEN-LAST:event_RegistrarMouseClicked
 
-    private void jButton3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseClicked
+    private void ReporteMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_ReporteMouseClicked
+        try {
+            conexion = new Conexion();
+            //conection = (Connection) conexion.getConnection();
+            String path="/home/labdessw09/NetBeansProjects/NaveEspacial/src/unsis/edu/mx/nave_espacial_reporte/report1.jasper";
+            JasperReport jr=null;
+            System.out.println("xgqsxgjksbx");
+            jr=(JasperReport) JRLoader.loadObjectFromFile(path);
+            System.out.println("kkkkkk");
+            net.sf.jasperreports.engine.JasperPrint jp=JasperFillManager.fillReport(jr,null,conexion.getConnection());
+            JasperViewer jv= new JasperViewer(jp);
+            jv.setVisible(true);
+            jv.setTitle(path);
+            conexion.close();
+        } catch (Exception e) {
+        }
+    }//GEN-LAST:event_ReporteMouseClicked
+
+    private void jugarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jugarMouseClicked
+      new Juego(idJugador).start();
+      this.setVisible(false);
+    }//GEN-LAST:event_jugarMouseClicked
+
+    private void RegistrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RegistrarActionPerformed
         // TODO add your handling code here:
+    }//GEN-LAST:event_RegistrarActionPerformed
+
+    private void JugadorMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_JugadorMouseClicked
+       // TODO add your handling code here:
         tablaJugador = new TablaJugador();
         this.setVisible(false);
         tablaJugador.setVisible(true);
-    }//GEN-LAST:event_jButton3MouseClicked
+    }//GEN-LAST:event_JugadorMouseClicked
 
-    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
-      new Juego(idJugador).start();
-      this.setVisible(false);
-    }//GEN-LAST:event_jButton1MouseClicked
+    private void ReporteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ReporteActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ReporteActionPerformed
 
     public static void main(String args[]) {
 
@@ -157,13 +214,14 @@ public class Menu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
+    private javax.swing.JButton Consultar;
+    private javax.swing.JButton Jugador;
+    private javax.swing.JButton Registrar;
+    private javax.swing.JButton Reporte;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton jugar;
     // End of variables declaration//GEN-END:variables
 }
